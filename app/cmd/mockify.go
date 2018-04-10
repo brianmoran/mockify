fd8e274091c033da821c911382d552cf5e3db40c
Brian Moran <bmoran@urbn.com>
2018-04-09 22:32:12 -0400

    Add multiple responses feature

--- app/cmd/mockify.go
+++ app/cmd/mockify.go
@@ -2,32 +2,40 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"os"
-	log "github.com/sirupsen/logrus"
-	"github.com/gorilla/mux"
 )
 
 type Route struct {
-	Path     string `json:"path"`
-	Methods   []string `json:"methods"`
-	ResponsePath string `json:"responsePath"`
+	Path         string   `json:"path"`
+	Methods      []string `json:"methods"`
+	ResponsePath string   `json:"responsePath"`
 }
 
 type Config struct {
-	Port string `json:"port"`
+	Port   string  `json:"port"`
 	Routes []Route `json:"routes"`
 }
 
 type Response struct {
-	StatusCode int `json:"statusCode"`
-	Body interface{} `json:"body"`
-	Headers map[string]string
+	Method     string      `json:"method"`
+	URI        string      `json:"uri"`
+	StatusCode int         `json:"statusCode"`
+	Body       interface{} `json:"body"`
+	Headers    map[string]string
 }
 
+type Responses struct {
+	Responses []Response `json:"responses"`
+}
+
+var responseMapping = make(map[string]Response)
+
 func loadConfig(path string) Config {
-	log.Infof("Looking for routes.json file in  %s/app directory\n", path)
+	log.Infof("Looking for routes.json file in  %s/app directory", path)
 	jsonFile, err := ioutil.ReadFile("app/routes.json")
 	if err != nil {
 		log.Errorf("Unable to parse file routes.json")
@@ -43,15 +51,34 @@ func loadConfig(path string) Config {
 	return config
 }
 
-func (route *Route) routeHandler(w http.ResponseWriter, r *http.Request) {
+func (route *Route) prefetchResponses() {
 	rawData, err := ioutil.ReadFile(route.ResponsePath)
 	if err != nil {
 		log.Errorf("Unable to open response file %s", route.ResponsePath)
 		os.Exit(3)
 	}
-	var response = Response{}
-	json.Unmarshal(rawData, &response)
-	log.Infof("%+v\n", response)
+
+	var responses Responses
+	json.Unmarshal(rawData, &responses)
+
+	for _, r := range responses.Responses {
+		key := r.Method + r.URI
+		responseMapping[key] = r
+	}
+}
+
+func (route *Route) routeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Infof("REQUEST: %+v %+v", r.Method, r.RequestURI)
+
+	log.Infof("ResponseMapping: %+v", responseMapping)
+	key := r.Method + r.RequestURI
+	response, ok := responseMapping[key]
+	if !ok {
+		log.Errorf("Response not mapped for method %s and URI %s", r.Method, r.RequestURI)
+		return
+	}
+
+	log.Infof("RESPONSE: %+v", response)
 
 	for k, v := range response.Headers {
 		w.Header().Add(k, v)
@@ -60,8 +87,8 @@ func (route *Route) routeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(response.StatusCode)
 
 	var body []byte
-	body, err = json.Marshal(response.Body)
-	if err!= nil {
+	body, err := json.Marshal(response.Body)
+	if err != nil {
 		log.Errorf("Unable to marshall body: %s", response.Body)
 		os.Exit(5)
 	}
@@ -74,6 +101,7 @@ func NewMockify() {
 
 	router := mux.NewRouter()
 	for _, route := range config.Routes {
+		route.prefetchResponses()
 		router.HandleFunc(route.Path, route.routeHandler).Methods(route.Methods...)
 	}
 
